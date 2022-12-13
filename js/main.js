8018138f2c20dec25d3ee8a39ff36faa8b0bf503
Eugene Kovgan <jugin.kovgan87@gmail.com>
2022-12-13 12:32:17 +0300

    feat: updated cv markdown

--- js/main.js
+++ js/main.js
@@ -1,9 +1,9 @@
 const closeBtn = document.querySelectorAll('.nav-link');
 const collapse = document.getElementById('navbarNavDropdown');
-console.log(collapse);
+// console.log(collapse);
 for (let i = 0; i < closeBtn.length; i++) {
   closeBtn[i].addEventListener('click', function () {
-    console.log('piu');
+    // console.log('piu');
     collapse.classList.remove('show');
   });
 }
